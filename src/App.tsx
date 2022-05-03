f5cc60c0ce7971d3a6ac95b0e65de67c160dc514
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-05-03 15:54:20 -0300

    Create the index.css configurations classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { PomodoroTimer } from './components/pomodoro-timer';
+import { PomodoroTimer } from './components/PomodoroTimer';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500} />
+    <div className="container">
+      <PomodoroTimer
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
     </div>
   );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { secondsToTime } from '../utils/seconds-to-time';
+import { secondsToTime } from '../utils/SecondsToTime';
 
 interface Props {
   mainTime: number;
